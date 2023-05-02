0481fe10e2698ce4b2f0d646c0b875881f96aa4e
Faiz_Hussaini <107918420+faizhussaini@users.noreply.github.com>
2023-05-03 01:59:14 +0530

    sim sim

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import vg from '../assets/2.webp'
+const Home = () => {
+  return (
+    <>
+    <div className='home'>
+    <main>
+        <h1> Technotron </h1>
+        <p> Lets Start a Journey Towards Future</p>
+    </main>
+    </div>
+
+    <div className='home2'>
+        <img src={vg} alt="" />
+        <div>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
